accf8284f2539fb92d938da3211b0792307b4bc3
Jose Barranco <jdcoding01@gmail.com>
2018-09-20 10:16:07 -0400

    Update newUser.php

--- web/views/newUser.php
+++ web/views/newUser.php
@@ -5,10 +5,14 @@
 <link rel="icon" href='/assets/talented.png'>
   <script src="/scripts/newUser.js"></script>
   <style>
-    body {background: "darkred";}
+    body {background-color: "darkred";}
   </style>
 </head>
 <body>
   <center><h1 style="color: darkcyan">TEST</h1></center>
+  <p>Welcome, $_POST['name']; </p>
+  <?php
+  echo "<p>PHP Test</p>";
+  ?>
 </body>
 </html>
