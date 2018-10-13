0045f83fe7eb65615db1987f333de1ebb6b12a81
Jose Barranco <jdcoding01@gmail.com>
2018-10-13 09:21:40 -0400

    Create contribute.php

---
+++ web/views/contribute.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Talented-- Contribute</title>
+<link rel="icon" href="/assets/talented.png">
+<link rel="stylesheet" type="text/css" href="/stylesheets/contribute.css">
+<script src="/scripts/contribute.js"></script>
+</head>
+<body>
+</body>
+</html>
+<!-- Still in development -->
+ 
